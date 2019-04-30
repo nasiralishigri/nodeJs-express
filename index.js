f862fc020610502e1a5ca382a8aa88a92741c3a4
nasiralishigri <nasiralishigri786@gmail.com>
2019-04-30 11:52:27 +0500

    Morgan module in express

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const http = require('http');
+const express = require('express'),
+     http = require('http');
 
+const hostname = 'localhost';
+const port = 3000;
+const morgan = require('morgan');
 
 const app = express();
-const port = 3000;
-const hostName = 'loclhost';
+app.use(morgan('dev'));
+app.use(express.static(__dirname +'/public'));
 
-
-app.use((req , res , next)=>{
-    
-    console.log(req.headers);
-    // res.statusCode= 200;
-    // res.setHeader('Content-Type', 'text/html');
-    // res.end('<html><body><h1> This is express side running</h1></body></html>');
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
 
 const server = http.createServer(app);
-server.listen(port ,hostName,() =>{
-    console.log("Your server is listen on http://" + hostName + ":" + "3000");
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
